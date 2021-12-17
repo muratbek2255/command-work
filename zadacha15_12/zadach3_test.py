f07b299016d50cd03760927dbedea6b3eb7a8346
Cholpon Omurbekova <cholpon.omurbekova@gmail.com>
2021-12-17 22:07:58 +0600

    Задание 3  и его тест

---
+++ zadacha15_12/zadach3_test.py
@@ -0,0 +1,18 @@
+import unittest
+from list16 import get_extention
+
+
+class Extensions (unittest.TestCase):
+    def setUp(self) -> None:
+        self.succesful_data = ["da.docx", "df.txt", "dj.csv"]
+        self.succesful = ['docx', 'txt', 'csv']
+        self.wrong_data = ["da.docx", "df", "dj1.csv"]
+
+    def test_succesful(self):
+        self.assertEqual(get_extention(self.succesful_data), self.succesful)
+    def test_wrong(self):
+        with self.assertRaises(EOFError) as context:
+            get_extention(self.wrong_data)
+
+if __name__ == '__main__':
+    unittest.main()
